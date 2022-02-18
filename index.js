cb82e89bf8b3e126b089bb9e8687643289f6d993
Veronica Leung <tungtungleung233@hotmail.com>
2022-02-17 22:34:37 -0500

    Emit chat message event and console log message

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  
   socket.on('disconnect', () => {
     console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
 });
 
